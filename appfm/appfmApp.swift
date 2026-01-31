335f1875f193baf82e28446d3cf7f81f122ce494
Konrad <kalembka.ko25@gmail.com>
2026-01-31 14:18:40 +0100

    Initial Commit

---
+++ appfm/appfmApp.swift
@@ -0,0 +1,17 @@
+//
+//  appfmApp.swift
+//  appfm
+//
+//  Created by Konrad Kalembka on 31/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct appfmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
